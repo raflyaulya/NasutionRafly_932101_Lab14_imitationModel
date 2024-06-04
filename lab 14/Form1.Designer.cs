d85db22ff8704a34f54c09f99c4b86559dbe5647
raflyaulya <auliarafli819@gmail.com>
2024-06-05 00:38:11 +0700

    Initial commit

---
+++ lab 14/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace lab_14
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(154, 13);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1148, 533);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 93);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 69);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1347, 559);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Bank Simulation";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ lab 14/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace lab_14
+{
+    public partial class Form1 : Form
+    {
+        private Bank bank;
+        private int clientId;
+        private Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSimulation();
+        }
+
+        private void InitializeSimulation()
+        {
+            bank = new Bank(3); // 3 operators
+            clientId = 1;
+            random = new Random();
+            InitializeDataGridView();
+        }
+
+        private void InitializeDataGridView()
+        {
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Client ID";
+            dataGridView1.Columns[1].Name = "Operator ID";
+            dataGridView1.Columns[2].Name = "Status";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (random.NextDouble() < 0.7) // 70% chance a new client arrives each second
+            {
+                bank.AddClient(new Client(clientId++, DateTime.Now));
+            }
+            bank.ProcessQueue();
+            bank.CheckOperators();
+            UpdateDataGridView();
+        }
+
+        private void UpdateDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+
+            foreach (var client in bank.GetQueue())
+            {
+                dataGridView1.Rows.Add(client.Id, "", "Waiting");
+            }
+
+            foreach (var op in bank.GetOperators())
+            {
+                if (op.IsBusy)
+                {
+                    dataGridView1.Rows.Add("", op.Id, "Serving");
+                }
+                else
+                {
+                    dataGridView1.Rows.Add("", op.Id, "Free");
+                }
+            }
+        }
+    }
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public DateTime ArrivalTime { get; set; }
+
+        public Client(int id, DateTime arrivalTime)
+        {
+            Id = id;
+            ArrivalTime = arrivalTime;
+        }
+    }
+
+    public class Operator
+    {
+        public int Id { get; set; }
+        public bool IsBusy { get; set; }
+        public DateTime FreeAt { get; set; }
+
+        public Operator(int id)
+        {
+            Id = id;
+            IsBusy = false;
+        }
+
+        public void ServeClient(Client client, int serviceTime)
+        {
+            IsBusy = true;
+            FreeAt = DateTime.Now.AddSeconds(serviceTime);
+        }
+
+        public void Free()
+        {
+            IsBusy = false;
+        }
+    }
+
+    public class Bank
+    {
+        private Queue<Client> queue = new Queue<Client>();
+        private List<Operator> operators = new List<Operator>();
+
+        public Bank(int operatorCount)
+        {
+            for (int i = 0; i < operatorCount; i++)
+            {
+                operators.Add(new Operator(i + 1));
+            }
+        }
+
+        public void AddClient(Client client)
+        {
+            queue.Enqueue(client);
+            Console.WriteLine($"Client {client.Id} arrived at {client.ArrivalTime}");
+        }
+
+        public void ProcessQueue()
+        {
+            while (queue.Count > 0)
+            {
+                var client = queue.Peek();
+                var freeOperator = operators.FirstOrDefault(op => !op.IsBusy);
+
+                if (freeOperator != null)
+                {
+                    int serviceTime = new Random().Next(5, 15); // Simulating service time between 5 to 15 seconds
+                    freeOperator.ServeClient(client, serviceTime);
+                    queue.Dequeue();
+                    Console.WriteLine($"Client {client.Id} is being served by Operator {freeOperator.Id} for {serviceTime} seconds.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public void CheckOperators()
+        {
+            foreach (var op in operators)
+            {
+                if (op.IsBusy && DateTime.Now >= op.FreeAt)
+                {
+                    op.Free();
+                    Console.WriteLine($"Operator {op.Id} is now free.");
+                }
+            }
+        }
+
+        public List<Client> GetQueue()
+        {
+            return queue.ToList();
+        }
+
+        public List<Operator> GetOperators()
+        {
+            return operators;
+        }
+    }
+}
